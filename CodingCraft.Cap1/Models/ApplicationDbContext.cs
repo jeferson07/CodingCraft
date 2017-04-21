b294dfd8b295b093493eb7e0804753b54b97bce4
Jeferson Almeida <jefhenrique07@gmail.com>
2017-04-21 14:35:55 -0300

    Adicionado overide no save para salvar automaticamente os campos das entidades editaveis e não editaveis

--- CodingCraft.Cap1/Models/ApplicationDbContext.cs
+++ CodingCraft.Cap1/Models/ApplicationDbContext.cs
@@ -4,7 +4,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Data.Entity.ModelConfiguration.Conventions;
-using CodingCraft.Cap1.Models.Classes;
+using CodingCraft.Cap1.Models.Entidades;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.Validation;
 
 namespace CodingCraft.Cap1.Models
 {
@@ -36,5 +42,84 @@ namespace CodingCraft.Cap1.Models
             base.OnModelCreating(dbModelBuilder);
             dbModelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
+
+        public override int SaveChanges()
+        {
+            try
+            {
+                CheckEntities(ChangeTracker.Entries());
+                return base.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                var errorMessages = ex.EntityValidationErrors
+                    .SelectMany(x => x.ValidationErrors)
+                    .Select(x => x.ErrorMessage);
+
+                var fullErrorMessage = string.Join(";", errorMessages);
+
+                var excpetionsMessage = string.Concat(ex.Message, "Os erros de validações são: ", fullErrorMessage);
+
+                throw new DbEntityValidationException(excpetionsMessage, ex.EntityValidationErrors);
+            }            
+        }
+
+        public override Task<int> SaveChangesAsync()
+        {
+            try
+            {
+                CheckEntities(ChangeTracker.Entries());
+                return base.SaveChangesAsync();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                var errorMessages = ex.EntityValidationErrors
+                    .SelectMany(x => x.ValidationErrors)
+                    .Select(x => x.ErrorMessage);
+
+                var fullErrorMessage = string.Join(";", errorMessages);
+
+                var excpetionsMessage = string.Concat(ex.Message, "Os erros de validações são: ", fullErrorMessage);
+
+                throw new DbEntityValidationException(excpetionsMessage, ex.EntityValidationErrors);
+            }
+        }
+
+        private void CheckEntities(IEnumerable<DbEntityEntry> entires)
+        {
+            var dataAtual = DateTime.Now;
+
+            foreach (var entry in entires.Where(e => e.Entity != null &&
+                        typeof(EntidadeNaoEditavel).IsAssignableFrom(e.Entity.GetType())))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    if (entry.Property("DataCriacao") != null)
+                    {
+                        entry.Property("DataCriacao").CurrentValue = dataAtual;
+                    }
+                    if (entry.Property("UsuarioCriacao") != null)
+                    {
+                        entry.Property("UsuarioCriacao").CurrentValue = HttpContext.Current != null ? HttpContext.Current.User.Identity.Name : "" ;
+                    }
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCriacao").IsModified = false;
+                    entry.Property("UsuarioCriacao").IsModified = false;
+
+                    if (entry.Property("DataModificacao") != null)
+                    {
+                        entry.Property("DataModificacao").CurrentValue = dataAtual;
+                    }
+                    if (entry.Property("UsuarioModificacao") != null)
+                    {
+                        entry.Property("UsuarioModificacao").CurrentValue = HttpContext.Current != null ? HttpContext.Current.User.Identity.Name : "";
+                    }
+                }
+            }
+        }
     }
+
 }
