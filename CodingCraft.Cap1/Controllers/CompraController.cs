75be1837757ed5aeb75346d5a400a6828847b8e5
Jeferson Almeida <jefhenrique07@gmail.com>
2017-05-01 12:49:23 -0300

    Criado logica para adicionar, editar e remover produtos compras na edição

--- CodingCraft.Cap1/Controllers/CompraController.cs
+++ CodingCraft.Cap1/Controllers/CompraController.cs
@@ -60,6 +60,7 @@ namespace CodingCraft.Cap1.Controllers
             if (ModelState.IsValid)
             {
                 compra.CompraId = Guid.NewGuid();
+                compra.ProdutoCompras.Select(pc => { pc.ProdutoCompraId = Guid.NewGuid(); return pc; }).ToList();
                 db.Compras.Add(compra);
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
@@ -103,6 +104,20 @@ namespace CodingCraft.Cap1.Controllers
                 var list = compra.ProdutoCompras.Select(x => x.ProdutoCompraId);
                 var produtosCompraDelete = db.ProdutoCompras.Where(pc => pc.CompraId == compra.CompraId && !list.Contains(pc.ProdutoCompraId));
                 db.ProdutoCompras.RemoveRange(produtosCompraDelete);
+
+                foreach (var produtoCompra in compra.ProdutoCompras)
+                {
+                    if (produtoCompra.ProdutoCompraId == Guid.Empty)
+                    {
+                        produtoCompra.ProdutoCompraId = Guid.NewGuid();
+                        db.ProdutoCompras.Add(produtoCompra);
+                    }
+                    else
+                    {
+                        db.Entry(produtoCompra).State = EntityState.Modified;
+                    }
+                }
+
                 db.Entry(compra).State = EntityState.Modified;
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
@@ -147,7 +162,7 @@ namespace CodingCraft.Cap1.Controllers
             var model = new ProdutoCompra();
             model.Produto = await db.Produtos.FindAsync(id);
             model.ProdutoId = model.Produto.ProdutoId;
-            model.ProdutoCompraId = Guid.NewGuid();
+            //model.ProdutoCompraId = Guid.NewGuid();
 
             return PartialView("_PartialProdutoCompra",model);
         }
