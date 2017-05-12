b53a0f7d72be34873ef3c86003f4dbe7279b7a3b
Jeferson Almeida <jefhenrique07@gmail.com>
2017-05-11 23:31:50 -0300

    Adicionado commit inicial do capitulo 2

---
+++ CodingCraft.Cap2/Models/Entities/Teste.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodingCraft.Cap2.Models.Entities
+{
+    public class Teste
+    {
+        [Key]
+        public int MyProperty { get; set; }
+        [Required]
+        public String Nome { get; set; }
+    }
+}
